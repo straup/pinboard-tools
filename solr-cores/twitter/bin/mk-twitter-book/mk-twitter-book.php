135e8dfdaaa5949f148b17286cb84ff851b334e8
straup <straup@gmail.com>
2012-12-25 09:58:50 -0500

    default solr endpoint; include_epilogue

--- solr-cores/twitter/bin/mk-twitter-book/mk-twitter-book.php
+++ solr-cores/twitter/bin/mk-twitter-book/mk-twitter-book.php
@@ -27,14 +27,19 @@
 
 		$spec = array(
 			"output" => array("flag" => "o", "required" => 1, "help" => "The path for the final PDF file you're creating."),
-			"username" => array("flag" => "u", "required" => 0, "help" => "The username of the person whose Tweets you're creating a book of. Unfortunately this information is not store in individual Tweets so you need to define it explicitly if you want it to appear in the book."),
-			"year" => array("flag" => "y", "required" => 0, "help" => "The year of Tweets you're created a book of. Defaults to the current year."),
+			"username" => array("flag" => "u", "required" => 0, "help" => "The username of the person whose tweets you're creating a book of."),
+			"year" => array("flag" => "y", "required" => 0, "help" => "The year of tweets you're created a book of. Defaults to the current year."),
+			"solr" => array("flag" => "s", "required" => 0, "help" => "The Solr endpoint where your tweets are stored. Defaults to http://localhost:8983/solr/twitter/."),
 		);
 
 		$opts = cli_getopts($spec);
 
 		if (! isset($opts['year'])){
 			$opts['year'] = date('Y', time());
+		}
+
+		if (! isset($opts['solr'])){
+			$opts['solr'] = 'http://localhost:8983/solr/twitter/';
 		}
 
 		$tweets = fetch_tweets_from_solr($opts);
@@ -48,18 +53,26 @@
 
 	#######################################################
 
-	function fetch_tweets_from_solr($opts){
+	function fetch_tweets_from_solr($opts, $more=array()){
 
-		$tweets = array();
+		$defaults = array(
+			'include_epilogue' => 1,
+		);
+
+		$more = array_merge($defaults, $more);
+
+		$date_range = "[ {$opts['year']}-01-01T00:00:00Z TO {$opts['year']}-12-31T23:59:59Z ]";
 
 		$parts = array(
-			"created_at:[ {$opts['year']}-01-01T00:00:00Z TO {$opts['year']}-12-31T23:59:59Z ]",
-			"favorited:false",
+			"created_at" => $date_range,
+			"favorited" => "false",
 		);
 
 		if ($user = $opts['username']){
-			$parts[] = "username:{$user}";
+			$parts['username'] = $user;
 		}
+
+		$tweets = array();
 
 		$page = 1;
 		$per_page = 1000;
@@ -67,17 +80,24 @@
 
 		while ((! isset($page_count)) || ($page <= $page_count)){
 
+			$query = array();
+
+			foreach ($parts as $k => $v){
+				$query[] = "{$k}:{$v}";
+			}
+
 			$params = array(
-				'q' => implode(" AND ", $parts),
+				'q' => implode(" AND ", $query),
+				'sort' => 'created_at asc',
 			);
 
-			$more = array(
-				'solr_endpoint' => 'http://localhost:8983/solr/twitter/',
+			$_more = array(
+				'solr_endpoint' => $opts['solr'],
 				'per_page' => $per_page,
 				'page' => $page,
 			);
 
-			$rsp = solr_select($params, $more);
+			$rsp = solr_select($params, $_more);
 
 			if (! $rsp['ok']){
 				break;
@@ -89,6 +109,37 @@
 
 			$tweets = array_merge($tweets, $rsp['rows']);
 			$page += 1;
+		}
+
+		if ($more['include_epilogue']){
+
+			$next_year = $opts['year'] + 1;
+			$date_range = "[ {$next_year}-01-01T00:00:00Z TO * ]";
+
+			$parts['created_at'] = $date_range;
+
+			$query = array();
+
+			foreach ($parts as $k => $v){
+				$query[] = "{$k}:{$v}";
+			}
+
+			$params = array(
+				'q' => implode(" AND ", $query),
+				'sort' => 'created_at asc',
+			);
+
+			$_more = array(
+				'solr_endpoint' => $opts['solr'],
+				'per_page' => 1,
+				'page' => 1,
+			);
+
+			$rsp = solr_select($params, $_more);
+
+			if (($rsp['ok']) && (count($rsp['rows']))){
+				$tweets = array_merge($tweets, $rsp['rows']);
+			}
 		}
 
 		return $tweets;
